dd35ed4c5025a5c26d2c3417d1b7795196f1ab8b
Srithar <sritharkalimuthu@gmail.com>
2023-11-07 19:41:53 +0530

    Complete commit of Project

---
+++ staff/conf/config.php
@@ -0,0 +1,6 @@
+<?php
+    $dbuser="#######";	//Configure with correct username and password
+    $dbpass="#######";
+    $host="localhost";
+    $db="internetbanking_db";
+    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
